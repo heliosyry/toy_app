d09cb03967b9be3c49f9c7980b209f2ebfc3d053
enyou <enyouruanyang@gmail.com>
2017-04-18 10:46:49 +0000

    users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
     def hello
-    render html: "hello, world!"
+    render html: "hllo, world!"
   end
 end
